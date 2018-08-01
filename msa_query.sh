e3c87d558465e4876bd5bd49e1eb0b6e46a57583
Anthony <33858023+Anth0nyME@users.noreply.github.com>
2018-08-01 18:38:30 +0300

    Add files via upload

--- msa_query.sh
+++ msa_query.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # set -x
-# Version 1.0
+# Version 1.1 (01.08.2018)
 # mad@starokashirka.ru
 if [ "$5" = "" ]; then
-echo Need: address port login pasword target [+object number to get details]
+clear
+printf "Usage: address port login pasword target [and target number to get xml output]\n\n"
+printf "Supported targets:\nsystem, versions, controllers, power-supplies, ports, fans, pools, hosts,\ndisks, disk-statistics, vdisks, vdisk-statistics, volumes, volume-statistics,\ndisk-groups, disk-group-statistics\n\n"
+printf "Note: not all targets are supported by ALL MSA models, so you need to query it\n"
 exit
 fi
 
@@ -12,7 +15,6 @@ msa_port=$2
 msa_login=$3
 msa_passwd=$4
 msa_target=$5
-
 object_xml_block='{#HP_MSA_BLOCK}'
 object_target_id='{#HP_MSA_TARGET}'
 
@@ -20,6 +22,11 @@ if [ "$5" = "ports" ]; then
 string_filter_1='basetype="port".*oid="'
 string_filter_2='PROPERTY.name="port"'
 target_str_grep=$(printf '"[0-9]*"\\|>[A-B][0-9]<')
+
+elif [ "$5" = "disk-statistics" ]; then
+string_filter_1='basetype="disk-statistics".*oid="'
+string_filter_2='PROPERTY.name="location"'
+target_str_grep=$(printf '[0-9].[0-9]*')
 
 elif [ "$5" = "disks" ]; then
 string_filter_1='basetype="drives".*oid="'
@@ -47,8 +54,18 @@ string_filter_1='basetype="volumes".*oid="'
 string_filter_2='PROPERTY.name="volume-name"'
 target_str_grep=$(printf '"[0-9]*"\\|>.*[0-9A-Za-z]*<')
 
+elif [ "$5" = "volume-statistics" ]; then
+string_filter_1='basetype="volume-statistics".*oid="'
+string_filter_2='PROPERTY.name="volume-name"'
+target_str_grep=$(printf '"[0-9]*"\\|>.*[0-9A-Za-z]*<')
+
 elif [ "$5" = "disk-groups" ]; then
 string_filter_1='basetype="disk-groups".*oid="'
+string_filter_2='PROPERTY.name="name"'
+target_str_grep=$(printf '"[0-9]*"\\|>.*[0-9A-Za-z]*<')
+
+elif [ "$5" = "disk-group-statistics" ]; then
+string_filter_1='basetype="disk-group-statistics".*oid="'
 string_filter_2='PROPERTY.name="name"'
 target_str_grep=$(printf '"[0-9]*"\\|>.*[0-9A-Za-z]*<')
 
@@ -73,8 +90,24 @@ string_filter_2='name="[a-z]*-.-[a-z]*"'
 target_str_grep=$(printf '[0-9]')
 target_str_gsub="{gsub(\"[a-z]*-a-[a-z]*\", \"1\"); gsub(\"[a-z]*-b-[a-z]*\", \"0\"); print}"
 target_str_sort=true
-fi
 
+elif [ "$5" = "vdisk-statistics" ]; then
+string_filter_1='basetype="vdisk-statistics".*oid="'
+string_filter_2='PROPERTY.name="name"'
+target_str_grep=$(printf '"[0-9]*"\\|>[0-9A-Za-z-]*<')
+
+elif [ "$5" = "pools" ]; then
+string_filter_1='basetype="pools".*oid="'
+string_filter_2='PROPERTY.name="name"'
+target_str_grep=$(printf '"[0-9]*"\\|>[0-9A-Za-z-]*<')
+
+elif [ "$5" = "fans" ]; then
+string_filter_1='basetype="fan".*oid="'
+string_filter_2='PROPERTY.name="durable-id"'
+target_str_grep=$(printf '"[0-9]*"\\|>.*<')
+target_str_gsub="{gsub(\"[a-z]*_\", \"\"); print}"
+
+fi
     if [ -z "$target_str_gsub" ]; then
     target_str_select=$(printf "/$string_filter_1|$string_filter_2/ {print}")
 	else 
